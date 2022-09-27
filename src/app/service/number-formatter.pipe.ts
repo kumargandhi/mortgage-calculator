b180a077f99afdbf91f4723415c63df3491de95f
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2022-09-27 10:38:42 +0530

    Fix the bug for balance is 0

--- src/app/service/number-formatter.pipe.ts
+++ src/app/service/number-formatter.pipe.ts
@@ -5,6 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NumberFormatterPipe implements PipeTransform {
     transform(value: number | undefined): string {
+        if (value === 0) {
+            return '0';
+        }
         if (!value) {
             return '';
         }
